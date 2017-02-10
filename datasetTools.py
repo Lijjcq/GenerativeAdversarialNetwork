890d8d2c83273209ad45cff349a4db02bae65c31
Julien Despois <despoisj@f45c89a11143.ant.amazon.com>
2017-02-10 17:49:38 +0100

    For cifar

--- datasetTools.py
+++ datasetTools.py
@@ -7,7 +7,7 @@ from random import shuffle
 def load_dataset():
     #Load, normalize and reshape
     files = os.listdir("Data/")
-    files = [file for file in files if file.endswith(".jpg")]
+    files = [file for file in files if file.endswith(".png")]
     
     includeDisabled = False
     if not includeDisabled:
@@ -23,6 +23,75 @@ def load_dataset():
 
     X_train = np.array(X_train)
     return X_train
+
+
+# Loads the CIFAR-10 dataset
+def extractCIFAR10(display=False):
+    #Load, normalize and reshape
+    files = os.listdir("CIFAR-10/")
+    files = [file for file in files if file.startswith("data_batch_")]
+
+    for file in files:
+        data = unpickle("CIFAR-10/"+file)
+        images, labels = data['data'], data['labels']
+
+        #Keep boats
+        for i in range(len(images)):
+            label = labels[i]
+
+            if label == 8:
+                img = images[i]
+                r, g, b = img[:1024],img[1024:2048],img[2048:]
+                r = r.reshape([32,32])
+                g = g.reshape([32,32])
+                b = b.reshape([32,32])
+                img = cv2.merge((b,g,r))
+                cv2.imwrite("Data/{}_{}.png".format(file,i),img)
+
+                if display:
+                    cv2.imshow("LOL",img)
+                    cv2.waitKey()
+
+
+# Loads the CIFAR-10 dataset
+def extractCIFAR100(display=False):
+
+    fineLabels =  unpickle("CIFAR-100/meta")['fine_label_names']
+    print fineLabels
+
+    selectedClass = fineLabels.index('aquarium_fish')
+
+    #Load, normalize and reshape
+    files = os.listdir("CIFAR-100/")
+    files = [file for file in files if file.startswith("data_batch_")]
+
+    for file in files:
+        data = unpickle("CIFAR-100/"+file)
+        images, labels = data['data'], data['fine_labels']
+        #Keep boats
+        for i in range(len(images)):
+            label = labels[i]
+
+            if label == selectedClass:
+                img = images[i]
+                r, g, b = img[:1024],img[1024:2048],img[2048:]
+                r = r.reshape([32,32])
+                g = g.reshape([32,32])
+                b = b.reshape([32,32])
+                img = cv2.merge((b,g,r))
+                
+                cv2.imwrite("Data/{}_{}.png".format(file,i),img)
+
+                if display:
+                    cv2.imshow("LOL",img)
+                    cv2.waitKey()
+
+def unpickle(file):
+    import cPickle
+    fo = open(file, 'rb')
+    dict = cPickle.load(fo)
+    fo.close()
+    return dict
 
 def addNoise(img):
     noise = np.zeros(img.shape[1:], np.float)
@@ -59,8 +128,9 @@ def explore_data():
 
 
 if __name__ == "__main__":
+    extractCIFAR100(display=False)
     #load_dataset()
-    explore_data()
+    #explore_data()
 
 
 
